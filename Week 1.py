286d4170ec8490f5f4877e72ef72abf00db2dac3
Benjamin-Smithh <b.smith4@uq.net.au>
2022-07-27 13:34:59 +1000

    Created Forward Difference Formula

--- Week 1.py
+++ Week 1.py
@@ -1,1 +1,18 @@
-print('hello world')
+
+def f(x):
+    return x*x*x
+
+h = 0.1
+
+def f_forward_der(x):
+    ff_der = (f(x + h) - f(x))/h
+    return ff_der
+
+k = 4
+
+print('forward difference derivative = ', f_forward_der(k))
+
+    
+    
+    
+    
